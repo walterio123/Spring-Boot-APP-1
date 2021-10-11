04307aad2e91da5f8239f207e87447dc7d2ddbdd
Victor Schweizer <walterio12345@gmail.com>
2021-10-11 11:57:32 -0300

    First Commit

---
+++ src/main/java/SpringBootAPP1/SpringBootApp1Application.java
@@ -0,0 +1,13 @@
+package SpringBootAPP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApp1Application.class, args);
+	}
+
+}
